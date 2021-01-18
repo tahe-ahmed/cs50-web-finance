b7e579d8d6e8ab20dd7e99466141c0a2f1da11a9
TaheAhmed <taheahmed80780@gmail.com>
2021-01-18 23:39:42 +0100

    add the webconfig.py

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-import os
+from webconfig import API_KEY
 import requests
 import urllib.parse
 
@@ -40,7 +40,7 @@ def lookup(symbol):
 
     # Contact API
     try:
-        api_key = os.environ.get("API_KEY")
+        api_key = API_KEY
         response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
     except requests.RequestException:
